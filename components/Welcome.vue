2f8817fe9744fd81450384430118ca4a7cf7635f
wangjr <wangjr@getui.com>
2023-06-04 10:37:31 +0800

    [chore]: 引入swiper

--- components/Welcome.vue
+++ components/Welcome.vue
@@ -19,7 +19,7 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="reveal h-full w-full">
+  <!-- <div class="reveal h-full w-full">
     <div class="slides">
       <section>
         Slide 1
@@ -31,5 +31,29 @@ onMounted(() => {
         <section>Vertical Slide 2</section>
       </section>
     </div>
-  </div>
+  </div> -->
+
+  <Swiper
+    :modules="[SwiperAutoplay, SwiperEffectCreative]"
+    :slides-per-view="1"
+    :loop="true"
+    effect="creative"
+    :autoplay="{
+      delay: 8000,
+      disableOnInteraction: true,
+    }"
+    :creative-effect="{
+      prev: {
+        shadow: false,
+        translate: ['-20%', 0, -1],
+      },
+      next: {
+        translate: ['100%', 0, 0],
+      },
+    }"
+  >
+    <SwiperSlide v-for="slide in 10" :key="slide">
+      <strong>{{ slide }}</strong>
+    </SwiperSlide>
+  </Swiper>
 </template>
